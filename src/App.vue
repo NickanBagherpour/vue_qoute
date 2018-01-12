c659e3030e610f99cca046dd5a65204e9d82f2ec
Nickan <Nickan.bagherpour@gmail.com>
2018-01-12 19:16:52 +0330

    create NewQuote.vue file and work on it

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
     <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
   import QuoteGrid from "./components/QuoteGrid.vue";
+  import NewQuote from "./components/NewQuote.vue";
   export default {
     data: function() {
       return {
@@ -13,14 +15,19 @@
           "Just a Quote to see something",
           "Nam fugit possimus voluptas explicabo quia debitis omnis qui.",
           "Odit delectus libero repudiandae earum dolorem in corrupti quis."
-          ],
+        ],
         maxQuotes: 10
       };
     },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    },
     components: {
       appQuoteGrid: QuoteGrid,
-    //  appNewQuote: NewQuote,
-     // appHeader: Header
+      appNewQuote: NewQuote,
+      // appHeader: Header
     }
   };
 </script>
