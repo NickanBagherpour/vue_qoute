b8bb98adc1168149b8aa01552e49141237b51976
Nickan <Nickan.bagherpour@gmail.com>
2018-01-12 19:42:18 +0330

    create Header.vue and work on limition of quote , progressbar ,...

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on Quote to delete</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import QuoteGrid from "./components/QuoteGrid.vue";
   import NewQuote from "./components/NewQuote.vue";
+  import Header from "./components/Header.vue";
   export default {
     data: function() {
       return {
@@ -16,18 +23,24 @@
           "Nam fugit possimus voluptas explicabo quia debitis omnis qui.",
           "Odit delectus libero repudiandae earum dolorem in corrupti quis."
         ],
-        maxQuotes: 10
+        maxQuotes: 10,
       };
     },
     methods: {
       newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes){
+          return alert('Please delete some quotes first.');
+        }
         this.quotes.push(quote);
-      }
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      },
     },
     components: {
       appQuoteGrid: QuoteGrid,
       appNewQuote: NewQuote,
-      // appHeader: Header
+      appHeader: Header,
     }
   };
 </script>
