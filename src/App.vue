d683fc98aeac1e0ead6d51e196fb16720cba6963
Nickan <Nickan.bagherpour@gmail.com>
2018-01-12 14:43:17 +0330

    rename Quote.vue and QuoteGrid.vue files and fix some bugs.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div class="container">
-
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      maxQoutes: 10
+  import QuoteGrid from "./components/QuoteGrid.vue";
+  export default {
+    data: function() {
+      return {
+        quotes: [
+          "Just a Quote to see something",
+          "Nam fugit possimus voluptas explicabo quia debitis omnis qui.",
+          "Odit delectus libero repudiandae earum dolorem in corrupti quis."
+          ],
+        maxQuotes: 10
+      };
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+    //  appNewQuote: NewQuote,
+     // appHeader: Header
     }
-  }
-}
+  };
 </script>
 
 <style>
-
+  
 </style>
